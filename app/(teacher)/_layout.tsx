bbe1d3b8d4cad1b8c57f457ac62e1afd186606c5
Asif Ahmed <iasif065@gmail.com>
2026-05-03 00:33:35 +0600

    53

--- app/(teacher)/_layout.tsx
+++ app/(teacher)/_layout.tsx
@@ -41,7 +41,7 @@ export default function TeacherLayout() {
         backgroundColor: focused ? Colors.accentLight : 'transparent',
         borderRadius: 10,
         paddingHorizontal: 6,
-        height: 54,
+        height: 53,
         alignItems: 'center',
         justifyContent: 'center',
         minWidth: 50,
